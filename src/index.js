80214f0275d5ad1a6e92e2c82227b3a76615ede0
ganpeng <190875565@qq.com>
2021-08-27 23:34:02 +0800

    chore: 测试处理 css

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import './hello.js'
 import avatar  from './asset/img/avatar.png'
 import avatarMini  from './asset/img/avatar-mini.png'
 import insertImg from "./utils/insertImg.js";
+import './style/index.less'
 
-insertImg(avatar)
-insertImg(avatarMini)
+insertImg(avatar, 'avatar')
+insertImg(avatarMini, 'avatarMini')
 
 console.log('index')
