2f3d592dbeabf85554394ca1dfb68b03f8243d90
ganpeng <190875565@qq.com>
2021-08-27 20:46:20 +0800

    chore: 配置HtmlWebpackPlugin
    yarn add html-webpack-plugin -D

--- build/webpack.base.js
+++ build/webpack.base.js
@@ -1,6 +1,13 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
   // 入口
   entry: {
     index: './src/index.js',
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "public/index.html"
+    })
+  ]
 }
