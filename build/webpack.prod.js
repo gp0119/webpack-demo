c591273072506b345d70fbf094fc23548a47192d
ganpeng <190875565@qq.com>
2021-08-28 01:45:56 +0800

    chore(优化): 提取js

--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -11,7 +11,7 @@ module.exports = merge(base, {
   //生产环境的 output 需要通过 contenthash 值来区分版本和变动，可达到清缓存的效果，而本地环境为了构建效率，则不引人 contenthash。
   output: {
     // bundle 文件名称 【只有这里和开发环境不一样】
-    filename: '[name].[contenthash].bundle.js',
+    filename: 'js/[name].[contenthash].bundle.js',
     // bundle 文件路径
     path: appDist,
     // 编译前清除目录
