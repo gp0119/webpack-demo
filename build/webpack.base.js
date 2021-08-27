fc42187270b926519c2716c5134edcbb02f40882
ganpeng <190875565@qq.com>
2021-08-27 21:01:01 +0800

    chore: 添加 script 配置devServer
    yarn add webpack-dev-server -D

--- build/webpack.base.js
+++ build/webpack.base.js
@@ -6,6 +6,7 @@ module.exports = {
     index: './src/index.js',
   },
   plugins: [
+    // 以 public 下 index.html 为模板生成 html,自动引入 bundle
     new HtmlWebpackPlugin({
       template: "public/index.html"
     })
--- build/webpack.dev.js
+++ build/webpack.dev.js
@@ -17,4 +17,13 @@ module.exports = merge(base, {
     // 编译前清除目录
     clean: true
   },
+  devServer: {
+    port: 8080,
+    client: {
+      progress: true, // 显示打包的进度条
+    },
+    static: appDist, // 告诉服务器从哪里提供内容，只有在你想要提供静态文件时才需要
+    open: true, // 自动打开浏览器
+    compress: true // 启动 gzip 压缩
+  },
 })
