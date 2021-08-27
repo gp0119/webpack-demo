8cda73ac0bffbc8d02bcb971a9b8b6cb2f6309ae
ganpeng <190875565@qq.com>
2021-08-28 00:56:33 +0800

    chore(优化): 抽离重复代码

--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -73,6 +73,31 @@ module.exports = merge(base, {
           },
         },
       }),
-    ]
+    ],
+    // 抽离重复代码
+    // webpack 将根据以下条件自动拆分 chunks：
+    //
+    // 新的 chunk 可以被共享，或者模块来自于 node_modules 文件夹；
+    // 新的 chunk 体积大于 20kb（在进行 min+gz 之前的体积）；
+    // 当按需加载 chunks 时，并行请求的最大数量小于或等于 30；
+    // 当加载初始化页面时，并发请求的最大数量小于或等于 30；
+    // 注意：切记不要为 cacheGroups 定义固定的 name，
+    // 因为 cacheGroups.name 指定字符串或始终返回相同字符串的函数时，
+    // 会将所有常见模块和 vendor 合并为一个 chunk。
+    // 这会导致更大的初始下载量并减慢页面加载速度。
+    splitChunks: {
+      chunks: 'all',
+      // 重复打包问题
+      cacheGroups:{
+        vendors:{ // node_modules里的代码
+          test: /[\\/]node_modules[\\/]/,
+          chunks: "all",
+          // name: 'vendors', 一定不要定义固定的name
+          priority: 10, // 优先级
+          // 告诉 webpack 忽略 splitChunks.minSize、splitChunks.minChunks、splitChunks.maxAsyncRequests 和 splitChunks.maxInitialRequests 选项，并始终为此缓存组创建 chunk。
+          enforce: true
+        }
+      }
+    },
   }
 })
