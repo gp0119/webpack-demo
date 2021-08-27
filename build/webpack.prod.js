480c53c88603bf4062e1182dd69d522ab5bd1796
ganpeng <190875565@qq.com>
2021-08-28 00:39:22 +0800

    chore(优化): JS 压缩
    使用 TerserWebpackPlugin 来压缩 JavaScript。

--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -1,6 +1,7 @@
 const { merge } = require('webpack-merge')
 const base = require('./webpack.base.js')
 const { appDist, appSrc } = require("./path.js");
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = merge(base, {
   // 生产模式
@@ -45,4 +46,33 @@ module.exports = merge(base, {
       },
     ]
   },
+  optimization: {
+    minimizer: [
+      // 使用 TerserWebpackPlugin 来压缩 JavaScript。
+      // webpack5 自带最新的 terser-webpack-plugin，无需手动安装。
+      // TerserWebpackPlugin 默认就开启了多进程和缓存，无需再引入 ParallelUglifyPlugin。
+      new TerserPlugin({
+        parallel: 4,
+        terserOptions: {
+          parse: {
+            ecma: 8,
+          },
+          compress: {
+            ecma: 5,
+            warnings: false,
+            comparisons: false,
+            inline: 2,
+          },
+          mangle: {
+            safari10: true,
+          },
+          output: {
+            ecma: 5,
+            comments: false,
+            ascii_only: true,
+          },
+        },
+      }),
+    ]
+  }
 })
